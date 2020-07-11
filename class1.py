a3bd0c38f564b6fdc74d465bfb4d3b533c39a80f
Ke5hav <kingkeshav1321+git@gmail.com>
2020-07-11 21:26:10 +0530

    END 11/07/2020

--- class1.py
+++ class1.py
@@ -1,1 +1,13 @@
 #This is my first class on python.
+print("hello") 
+# this is my frst code
+
+# DATA Types:
+# 1. int,float, Complex
+# 2. String-> "<charecters set>"
+# 3. Boolean (true/false)
+# 4. list [a,b,c,d]
+# 5. tuple 
+# 6. Sets
+# 7. Dictionary
+
